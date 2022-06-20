88948e43a0dfba641451bdceb490e0bec67204ac
Artem Pistryi <gazated@gmail.com>
2022-06-20 22:24:15 +0300

    let's try

--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,14 @@
 package reverse_string
 
 func ReverseString(input string) (output string) {
-	// solution goes here
-	return output
+	if input == "" {
+		return
+	}
+	arr := []rune(input)
+	length := len(arr)
+	for i := 0; i < length/2; i++ {
+		arr[i], arr[length-i-1] = arr[length-i-1], arr[i]
+	}
+	output = string(arr)
+	return
 }
